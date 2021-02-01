371ff3a835dc1248eb1559413d7066c7ea0e29ce
mvictor-vg <60658435+mvictor-vg@users.noreply.github.com>
2021-02-01 20:51:14 -0300

    Nothing special

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const express = require("express");
 const app = express();
 const messageRoutes = require("./routes/messageRoutes");
+const emailRoutes = require('./routes/emailRoutes');
+
 
 const port = 6005;
 
@@ -10,6 +12,7 @@ const setUpExpress = () => {
     app.use(express.json({ limit: '50mb' }));
     app.use(cors())
     app.use('/api/message', messageRoutes);
+    app.use('/api/email', emailRoutes);
 
     app.listen(port, () => {
         console.log(`Server running at: http://localhost:${port}`)
---
+++ controllers/emailController.js
@@ -0,0 +1,8 @@
+const nodeMailer = require('nodemailer')
+
+
+const sendEmail = async(messageBody) => {
+    
+
+
+}
