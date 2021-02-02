0073abda902041ce0b518035bd395a4149fcbb8a
root <root@WKS147.vgaia2003.br>
2021-02-02 09:30:10 -0300

    Fixed error which stopped the app from running

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const express = require("express");
 const app = express();
 const messageRoutes = require("./routes/messageRoutes");
-const emailRoutes = require('./routes/emailRoutes');
+// const emailRoutes = require('./routes/emailRoutes');
 
 
 const port = process.env.PORT || 6005;
@@ -12,7 +12,7 @@ const setUpExpress = () => {
     app.use(express.json({ limit: '50mb' }));
     app.use(cors())
     app.use('/api/message', messageRoutes);
-    app.use('/api/email', emailRoutes);
+    // app.use('/api/email', emailRoutes);
 
     app.listen(port, () => {
         console.log(`Server running at: http://localhost:${port}`)
